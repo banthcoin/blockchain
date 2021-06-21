f65737f7311bead6f8f15fdd45d6bb4014ed76eb
Airton Espindola <airtonespind@gmail.com>
2021-06-21 13:54:23 -0300

    add first block

---
+++ database/seeders/FirstBlock.ts
@@ -0,0 +1,20 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Block from 'App/Models/Block'
+
+export default class FirstBlockSeeder extends BaseSeeder {
+  public async run() {
+    await Block.firstOrCreate(
+      { index: 1 },
+      {
+        timestamp: new Date().getTime(),
+        nonce: 0,
+        publicKey: 'first_block',
+        sender: 'first_block',
+        recipier: 'recipier',
+        amount: 100000000000000000,
+        hash: 'first_block',
+        signature: 'first_block',
+      }
+    )
+  }
+}
